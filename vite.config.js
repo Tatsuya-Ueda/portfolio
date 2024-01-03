349d38b0b5c5ad4cfab136f4a705861f1f4b7b32
t_ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2024-01-03 14:34:09 +0900

    Github Pagesへdeployするためのbaseを設定

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,6 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/myWiki/",
+  // Github Pagesへdeployするためのbaseを設定
+  base: "/portfolio/",
 });
