523988933e3ef5a20dcee04bf6f1fb5d54c71040
t_ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2024-01-08 22:27:20 +0900

    feat: 記事IDに対応したページを表示できるよう改造

---
+++ src/components/Post.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      content: "",
+    };
+  },
+  created() {
+    // viewが作成された直後にデータを取得する．
+    this.fetchData();
+  },
+  watch: {
+    $route: "fetchData",
+  },
+  methods: {
+    async fetchData() {
+      let res = await fetch("/portfolio/posts/dummy.json");
+      this.content = await res.json();
+    },
+  },
+};
+</script>
+
+<template>
+  <div>記事ID: {{ $route.params.post_id }}</div>
+  <div id="content">{{ content }}</div>
+</template>
+
+<style scoped></style>
