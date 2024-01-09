6662861b8c9de47fef178c4dd2e07e12c188ad8c
Tatsuya-Ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2024-01-09 20:45:01 +0900

    change: post.jsonのファイル名の英字は小文字で統一

--- scripts/mdToPostJSON.cjs
+++ scripts/mdToPostJSON.cjs
@@ -31,6 +31,6 @@ for (let i = 0; i < mdFiles.length; i++) {
   const text = fs.readFileSync(`${pathIn}/${mdFile}`, "utf-8");
   const html = marked.parse(text);
   const jsonStr = JSON.stringify({ html: html });
-  const filenameOut = getBaseName(mdFile) + ".json";
+  const filenameOut = getBaseName(mdFile).toLowerCase() + ".json";
   fs.writeFileSync(`${pathOut}/${filenameOut}`, jsonStr);
 }
